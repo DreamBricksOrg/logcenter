42700000b03919ba6f7f311ae709fb505d7051a7
Guilherme Begotti <domingos.guilherme@gmail.com>
2025-09-16 16:52:57 -0300

    fix: locals files

--- src/main.py
+++ src/main.py
@@ -19,7 +19,3 @@ app.include_router(projects.router)
 def health():
     return {"status": "ok"}
 
-if __name__ == "__main__":
-    import os
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
